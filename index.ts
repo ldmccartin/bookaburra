101afc496afd709ea9525dca13f61ba1805a2f73
Luke McCartin <ldmccartin@gmail.com>
2024-07-17 20:19:26 +0200

    db changes, try on stop

--- index.ts
+++ index.ts
@@ -3,28 +3,28 @@ import { rateLimit } from 'elysia-rate-limit'
 import * as mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
 
+import { getAll, save } from './src/db/resource';
+type resourcePost = { body: { name: string, url: string } };
 dotenv.config();
-
-import { getAll } from './src/db/resource';
 
 await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qwzopp0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
 
-const allResources = await getAll();
-
-console.log('all resources', allResources);
-
-await mongoose.disconnect();
-
 const app = new Elysia().use(rateLimit());
 
-app.get("/", async () => {
+app.get('/', async () => {
   try {
-    return new Response(JSON.stringify(allResources));
+    return new Response(JSON.stringify("Bookaburra"));
   } catch (error: any) {
     return new Response(error.message, { status: 500 });
   }
 });
+app.get('/all', async () => await getAll());
+app.post('/resource', async ({ body: { name, url } }: resourcePost) => await save(name, url));
 
 app.listen(3000, () => {
-  console.log("Server running on port 3000");
+  console.log('Server running...');
+}).onStop(async () => {
+  console.log('Server shutting down...')
+  await mongoose.disconnect();
+  console.log('Server stopped.')
 });
