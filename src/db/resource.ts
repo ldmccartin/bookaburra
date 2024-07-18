0fe387c01283887c040812b6945469b9a2b06115
Luke McCartin <ldmccartin@gmail.com>
2024-07-18 22:23:10 +0200

    get og data

--- src/db/resource.ts
+++ src/db/resource.ts
@@ -1,4 +1,13 @@
+import ogs from 'open-graph-scraper';
 import { Resource } from './schema/schema';
+
+import type { SuccessResult } from 'open-graph-scraper'
+
+export interface resource {
+  name: string;
+  url: string;
+  ogData?: SuccessResult['result']
+}
 
 export const save = async (name: string, url: string) => {
   const resource = new Resource({ name, url });
@@ -6,5 +15,11 @@ export const save = async (name: string, url: string) => {
 }
 
 export const getAll = async () => {
- return Resource.find().lean();
+  const resources = Resource.find().lean();
+  const mappedResources: Array<resource> = [];
+  for await (const resource of resources) {
+    const { result } = await ogs({ url: resource.url});
+    mappedResources.push({...resource, ogData: result})
+  }
+  return mappedResources;
 }
